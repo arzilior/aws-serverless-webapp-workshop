e6a997b26c028daa54db09dbbed384e01f97973b
arzilior <81254154+arzilior@users.noreply.github.com>
2021-05-08 21:30:33 +0300

    Update config.js
    added the call to the Lamda API

--- resources/code/WildRydesVue/src/config.js
+++ resources/code/WildRydesVue/src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://ss7ez8qg13.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
